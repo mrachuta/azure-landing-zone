a7c69e6b62ac1fe40e175c75946644573910f32c
mrachuta <34251394+mrachuta@users.noreply.github.com>
2026-03-16 23:38:17 +0100

    Remove old resources

--- modules/project/data.tf
+++
@@ -1,5 +0,0 @@
-data "azurerm_subnet" "vngtest_subnet" {
-  name                 = var.vngtest_subnet_name
-  virtual_network_name = var.project_network_name
-  resource_group_name  = var.project_resource_group_name
-}
